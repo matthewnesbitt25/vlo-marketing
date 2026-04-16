3cc4c99544a564e9be7ed39c99bf33833cfc090f
matthewnesbitt <matthewnesbitt25@gmail.com>
2026-04-16 09:23:43 -0400

    vlo logo

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "@/public/images/logo.svg";
+import logo from "@/public/images/Transparent.png";
 
 export default function Logo() {
   return (
-    <Link href="/" className="inline-flex shrink-0" aria-label="Cruip">
-      <Image src={logo} alt="Cruip Logo" width={32} height={32} />
+    <Link href="/" className="inline-flex shrink-0" aria-label="VLOStats">
+      <Image 
+        src={logo} 
+        alt="VLOStats Logo" 
+        width={38} 
+        height={38} 
+        priority 
+      />
     </Link>
   );
 }
