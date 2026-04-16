b0b73a39e2391621d8d9ac5a4667280be760c354
matthewnesbitt <matthewnesbitt25@gmail.com>
2026-04-16 09:25:19 -0400

    header edit

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
           <ul className="flex flex-1 items-center justify-end gap-3">
             <li>
               <Link
-                href="/signin"
+                href="https://vlostats.app/login"
                 className="btn-sm relative bg-linear-to-b from-gray-800 to-gray-800/60 bg-size-[100%_100%] bg-bottom py-1.25 text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,var(--color-gray-800),var(--color-gray-700),var(--color-gray-800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,linear-gradient(white_0_0)] hover:bg-size-[100%_150%]"
               >
                 Sign In
@@ -25,10 +25,10 @@ export default function Header() {
             </li>
             <li>
               <Link
-                href="/signup"
+                href="https://vlostats.app/signup"
                 className="btn-sm bg-linear-to-t from-indigo-600 to-indigo-500 bg-size-[100%_100%] bg-bottom py-1.25 text-white shadow-[inset_0px_1px_0px_0px_--theme(--color-white/.16)] hover:bg-size-[100%_150%]"
               >
-                Register
+                Get Started
               </Link>
             </li>
           </ul>
